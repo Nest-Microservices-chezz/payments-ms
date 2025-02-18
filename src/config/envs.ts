a4bacaba787c4ad9e3046f48d46be689e03104b2
Grecia Delgado <greciadm2596@gmail.com>
2025-02-18 12:59:51 -0500

    Add stripe SDK

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,12 +3,15 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  STRIPE_SECRET: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    STRIPE_SECRET: joi.string().required(),
   })
+
   .unknown(true);
 
 const { error, value } = envsSchema.validate({
@@ -23,4 +26,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  stripeSecret: envVars.STRIPE_SECRET,
 };
