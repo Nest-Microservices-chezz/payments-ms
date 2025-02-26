7025165c37778772ee016bf1873261dfbb774008
Grecia Delgado <greciadm2596@gmail.com>
2025-02-26 12:34:21 -0500

    Add NTS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,8 @@ interface EnvVars {
   STRIPE_SUCESS_URL: string;
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
@@ -16,12 +18,14 @@ const envsSchema = joi
     STRIPE_SUCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
 
   .unknown(true);
 
 const { error, value } = envsSchema.validate({
   ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) {
@@ -36,4 +40,5 @@ export const envs = {
   stripeSuccessUrl: envVars.STRIPE_SUCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
   stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
+  natsServers: envVars.NATS_SERVERS,
 };
