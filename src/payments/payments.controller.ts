b7e8596c6a1e07f218eef9aa0bf271f2236e3639
Grecia Delgado <greciadm2596@gmail.com>
2025-03-14 17:00:06 -0500

    Configure Dockerfile Prod

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -7,6 +7,7 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
+  @Post('create-payment-session')
   @MessagePattern('create.payment.session')
   createPaymentSession(@Payload() paymentSessionDto: PaymentSessionDto) {
     return this.paymentsService.createPaymentSession(paymentSessionDto);
